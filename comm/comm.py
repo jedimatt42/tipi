9bda9d16d8ce62eba2ed6d8e2c0e2993dd18bd32
Matthew Splett <matt@cwfk.net>
2017-01-08 05:28:31 +0000

    Update to use bitwise shift operators.
    This is more concise. now that I get what is what with the GPIO library.

--- comm/comm.py
+++ comm/comm.py
@@ -32,14 +32,14 @@ while True:
 
     byte = 0
 
-    byte += 1 if GPIO.input(BIT0) == True else 0
-    byte += 2 if GPIO.input(BIT1) == True else 0
-    byte += 4 if GPIO.input(BIT2) == True else 0
-    byte += 8 if GPIO.input(BIT3) == True else 0
-    byte += 16 if GPIO.input(BIT4) == True else 0
-    byte += 32 if GPIO.input(BIT5) == True else 0
-    byte += 64 if GPIO.input(BIT6) == True else 0
-    byte += 128 if GPIO.input(BIT7) == True else 0
+    byte += GPIO.input(BIT0) # << 0
+    byte += GPIO.input(BIT1) << 1
+    byte += GPIO.input(BIT2) << 2
+    byte += GPIO.input(BIT3) << 3
+    byte += GPIO.input(BIT4) << 4
+    byte += GPIO.input(BIT5) << 5
+    byte += GPIO.input(BIT6) << 6
+    byte += GPIO.input(BIT7) << 7
 
 #    print byte
 

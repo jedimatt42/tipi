50219bc39e4a183e8383ba309e1326d1b6309073
Matthew Splett <matt@cwfk.net>
2017-01-25 23:21:35 -0800

    latch 2 bytes, 5fff and 5ffd

--- cplds/mojo-ise/src/mojo_top.v
+++ cplds/mojo-ise/src/mojo_top.v
@@ -17,16 +17,47 @@ module mojo_top(
     // Serial connections
     input avr_tx, // AVR Tx => FPGA Rx
     output avr_rx, // AVR Rx => FPGA Tx
-    input avr_rx_busy // AVR Rx buffer full
+    input avr_rx_busy, // AVR Rx buffer full
+	 
+	 input tipi_data_out,
+	 input tipi_control_out,
+	 input tipi_dsr_out,
+	 
+	 input [0:15]ti_a,
+	 input [7:0]ti_data,
+	 input ti_memen,
+	 input ti_we,
+	 input [3:0]cru_base,
+	 input ti_dbin,
+	 input ti_cruclk,
+	 input ti_reset
     );
 
 wire rst = ~rst_n; // make reset active high
+reg [7:0] data_q;
+reg [7:0] control_q;
 
 // these signals should be high-z when not used
 assign spi_miso = 1'bz;
 assign avr_rx = 1'bz;
 assign spi_channel = 4'bzzzz;
 
-assign led = 8'b0;
+assign tipi_data_out = 1'b1;
+assign tipi_control_out = 1'b1;
+assign tipi_dsr_out = 1'b1;
+
+always @(negedge ti_we) begin
+  if (~ti_memen && ti_a == 16'h5fff) begin
+    data_q <= ti_data;
+  end
+  if (~ti_memen && ti_a == 16'h5ffd) begin
+    control_q <= ti_data;
+  end
+end
+
+
+
+assign led[7:4] = data_q[7:4];
+assign led[3:0] = control_q[3:0];
 
 endmodule

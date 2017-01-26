93aa22ad92b1013a3f7a0876e4c455b129d58107
Matthew Splett <matt@cwfk.net>
2017-01-25 23:38:09 -0800

    output to RPi

--- cplds/mojo-ise/src/mojo_top.v
+++ cplds/mojo-ise/src/mojo_top.v
@@ -19,9 +19,9 @@ module mojo_top(
     output avr_rx, // AVR Rx => FPGA Tx
     input avr_rx_busy, // AVR Rx buffer full
 	 
-	 input tipi_data_out,
-	 input tipi_control_out,
-	 input tipi_dsr_out,
+	 output tipi_data_out,
+	 output tipi_control_out,
+	 output tipi_dsr_out,
 	 
 	 input [0:15]ti_a,
 	 input [7:0]ti_data,
@@ -30,7 +30,9 @@ module mojo_top(
 	 input [3:0]cru_base,
 	 input ti_dbin,
 	 input ti_cruclk,
-	 input ti_reset
+	 input ti_reset,
+	 input [7:0]rpi_d,
+	 input [7:0]rpi_s
     );
 
 wire rst = ~rst_n; // make reset active high
@@ -49,15 +51,16 @@ assign tipi_dsr_out = 1'b1;
 always @(negedge ti_we) begin
   if (~ti_memen && ti_a == 16'h5fff) begin
     data_q <= ti_data;
-  end
+  end else 
   if (~ti_memen && ti_a == 16'h5ffd) begin
     control_q <= ti_data;
   end
 end
 
-
-
 assign led[7:4] = data_q[7:4];
 assign led[3:0] = control_q[3:0];
+
+assign rpi_d = data_q;
+assign rpi_s = control_q;
 
 endmodule

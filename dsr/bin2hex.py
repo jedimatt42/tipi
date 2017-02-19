d9b5b2d6afed267e387d32d97e92a787c0ed975c
Matthew Splett <matt@cwfk.net>
2017-02-18 23:09:36 -0800

    function calling convention

--- dsr/bin2hex.py
+++ dsr/bin2hex.py
@@ -4,9 +4,22 @@ import time
 from array import array
 
 fh = open("tipi.bin", 'rb')
+romSize = 8192
+
+# Weird bug work-around... not long term. need to fix verilog code
+while romSize > 7680:
+    romSize -= 1
+    print hex(0xFF)[2:].zfill(2)
+
 try:
     bytes = bytearray(fh.read())
     for byte in bytes:
         print hex(byte)[2:].zfill(2)
+        romSize -= 1
 finally:
     fh.close()
+
+while romSize > 0:
+    romSize -= 1
+    print hex(romSize >> 8 % 255)[2:].zfill(2)
+

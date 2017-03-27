b4b27af988dd9e6c5bc0e8a15f237ce582d3c4cb
Matthew Splett <matt@cwfk.net>
2017-03-27 05:57:31 +0000

    python change port code to output to CPLD shift registers, much slower

--- dsr/python/test1.py
+++ dsr/python/test1.py
@@ -4,12 +4,20 @@ import sys
 import time
 
 from tipi.TipiMessage import TipiMessage
+from tipi.TipiPorts import TipiPorts
 
 tipi_io = TipiMessage()
+tipi_ports = TipiPorts()
 
-bytes = tipi_io.receive()
+start = time.time()
 
-for byte in bytes:
-  print '{0:0>2x} '.format(byte)
-  sys.stdout.flush()
+for i in range(0,8192):
+    time.sleep(0.001)
+    tipi_ports.setRD(0)
+    time.sleep(0.001)
+    tipi_ports.setRD(i)
+
+stop = time.time()
+
+print stop - start
 

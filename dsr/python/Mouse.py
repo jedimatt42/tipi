5f09d1fb2811d6cfc5082ca36082dbf03c9c04dc
Matthew Splett <matt@cwfk.net>
2017-04-09 14:07:46 -0700

    non-blocking reads from mouse input.

--- dsr/python/Mouse.py
+++ dsr/python/Mouse.py
@@ -1,4 +1,6 @@
 import struct
+import fcntl
+import os
 
 # Issues: reading from file is blocking IO. 
 # Can't be blocking the TI-99/4A like that. 
@@ -8,16 +10,18 @@ class Mouse(object):
     def __init__(self, tipi_io):
         self.tipi_io = tipi_io
         self.file = open( "/dev/input/mice", "rb" );
+        fd = self.file.fileno()
+        flag = fcntl.fcntl(fd, fcntl.F_GETFL)
+        fcntl.fcntl(fd, fcntl.F_SETFL, flag | os.O_NONBLOCK)
 
     def getMouseEvent(self):
-	buf = self.file.read(3);
-	button = ord( buf[0] );
-	# bLeft = button & 0x1;
-	# bMiddle = ( button & 0x4 ) > 0;
-	# bRight = ( button & 0x2 ) > 0;
-	x,y = struct.unpack( "bb", buf[1:] );
-        # Probably could just skip the unpack and accept button as first byte.
-        return bytearray(struct.pack('bbb', x, -1 * y, button))
+        try:
+	  buf = self.file.read(3);
+	  button = ord( buf[0] );
+	  x,y = struct.unpack( "bb", buf[1:] );
+          return bytearray(struct.pack('bbb', x, -1 * y, button))
+        except IOError as e:
+          return bytearray([0,0,0])
 
     def handle(self, bytes):
         self.tipi_io.sendRaw(self.getMouseEvent())

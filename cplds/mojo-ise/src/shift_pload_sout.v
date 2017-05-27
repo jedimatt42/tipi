e6f8e80e8098f5014083515deadce494435c836e
Matthew Splett <matt@cwfk.net>
2017-05-27 15:10:24 -0700

    add the serial out module

---
+++ cplds/mojo-ise/src/shift_pload_sout.v
@@ -0,0 +1,30 @@
+`ifndef _shift_pload_sout_vh_
+`define _shift_pload_sout_vh_
+
+module shift_pload_sout (
+    // Clock for shifting
+    input clk,
+	 // Operations are ignored unless CS is high
+    input cs,
+	 // Load parallel data
+    input aload,
+	 // Data to load from
+    input [7:0]data,
+	 // output bit from the left.
+    output sout
+);
+
+reg [7:0]tmp;
+
+always @(posedge clk) begin
+    if (cs) begin
+        if (aload) tmp = data;
+        else tmp = {tmp[6:0], 1'b0};
+     end
+end
+
+assign sout = tmp[7];
+
+endmodule
+
+`endif

f46d59ce3187926c0006ae86c83bb77ed4298785
Matthew Splett <matt@cwfk.net>
2017-09-22 21:12:43 -0700

    Fix singleton code.

--- dsr/python/tipi/TipiPorts.py
+++ dsr/python/tipi/TipiPorts.py
@@ -6,13 +6,7 @@ import RPi.GPIO as GPIO
 
 logger = logging.getLogger(__name__)
 
-singleton = TipiPorts()
-
 class TipiPorts(object):
-
-    @staticmethod
-    def getInstance():
-        return singleton
 
     def __init__(self):
         logger.info("Using libtipi wiringPi GPIO")
@@ -36,3 +30,10 @@ class TipiPorts(object):
     # Write RPI_CONTROL
     def setRC(self, value):
         tipiports.setRC(value)
+
+    # Use this method to make sure we only
+    @staticmethod
+    def getInstance():
+        return singleton
+
+singleton = TipiPorts()

75910bdf88730c829a34ec36a8546b1da2563a88
Matthew Splett <matt@cwfk.net>
2017-11-04 08:17:34 -0700

    Set ssid and psk in wpa_supplicant through TipiSuper service.

--- services/TipiSuper.py
+++ services/TipiSuper.py
@@ -6,6 +6,8 @@ from subprocess import call
 
 while True:
     time.sleep(1)
+
+    # shutdown the PI safely
     if os.path.exists("/tmp/tipihalt"):
         callargs = ["/sbin/shutdown", "-h", "now"]
         if call(callargs) != 0:
@@ -14,6 +16,7 @@ while True:
             while True:
                 time.sleep(20)
         
+    # reboot the PI
     if os.path.exists("/tmp/tipireboot"):
         callargs = ["/sbin/shutdown", "-r", "now"]
         if call(callargs) != 0:
@@ -21,4 +24,35 @@ while True:
         else:
             while True:
                 time.sleep(20)
+
+    # Configure WiFi
+    wificonfig="/tmp/wificonfig"
+    if os.path.exists(wificonfig):
+        # wait a couple seconds so file will be complete.
+        time.sleep(2)
+        # expect two config entries
+        try:
+            # read the username and password
+            with open(wificonfig) as fh:
+                ssid = fh.readline().rstrip()
+                psk = fh.readline().rstrip()
+            
+            # remove handoff file
+            os.remove(wificonfig)
+
+            # adjust settings for network 0
+            with open("/etc/wpa_supplicant/wpa_supplicant.conf",'w') as fh_out:
+                with open("/home/tipi/tipi/services/templates/wpa_supplicant.conf") as fh_in:
+                    for line in fh_in:
+                        line = line.replace("${SSID}", ssid)
+                        line = line.replace("${PSK}", psk)
+                        fh_out.write(line)
+
+            # reload the new wpa_supplicant.conf
+            callargs = ["/sbin/wpa_cli", "reconfigure"]
+            if call(callargs) != 0:
+                raise Exception("failed to reload configuration")
+
+        except Exception as e:
+            print e
         

dd35c6b88a931bf2e80ba4f5dd2e9ba2880bb9a5
Corey Anderson <corey@electriclab.com>
2017-11-12 20:50:58 -0800

    Changes to support a better UX for managing files, and for creating/editing BASIC files - a staple of the TI 99 experience :) Added support in TipiSuper.py to allow for a bootstrap file to connect to a headless TIPI to one's WiFi.

--- services/TipiSuper.py
+++ services/TipiSuper.py
@@ -1,8 +1,40 @@
 #!/usr/bin/env python2
+#
+
 import os
 import sys
 import time
 from subprocess import call
+
+
+def configureWifi(wificonfig):
+    # wait a couple seconds so file will be complete.
+    time.sleep(2)
+    # expect two config entries
+    try:
+        # read the username and password
+        with open(wificonfig) as fh:
+            ssid = fh.readline().rstrip()
+            psk = fh.readline().rstrip()
+        
+        # remove handoff file
+        os.remove(wificonfig)
+
+        # adjust settings for network 0
+        with open("/etc/wpa_supplicant/wpa_supplicant.conf",'w') as fh_out:
+            with open("/home/tipi/tipi/services/templates/wpa_supplicant.conf") as fh_in:
+                for line in fh_in:
+                    line = line.replace("${SSID}", ssid)
+                    line = line.replace("${PSK}", psk)
+                    fh_out.write(line)
+
+        # reload the new wpa_supplicant.conf
+        callargs = ["/sbin/wpa_cli", "reconfigure"]
+        if call(callargs) != 0:
+            raise Exception("failed to reload configuration")
+
+    except Exception as e:
+        print e
 
 while True:
     time.sleep(1)
@@ -25,34 +57,13 @@ while True:
             while True:
                 time.sleep(20)
 
-    # Configure WiFi
-    wificonfig="/tmp/wificonfig"
-    if os.path.exists(wificonfig):
-        # wait a couple seconds so file will be complete.
-        time.sleep(2)
-        # expect two config entries
-        try:
-            # read the username and password
-            with open(wificonfig) as fh:
-                ssid = fh.readline().rstrip()
-                psk = fh.readline().rstrip()
-            
-            # remove handoff file
-            os.remove(wificonfig)
-
-            # adjust settings for network 0
-            with open("/etc/wpa_supplicant/wpa_supplicant.conf",'w') as fh_out:
-                with open("/home/tipi/tipi/services/templates/wpa_supplicant.conf") as fh_in:
-                    for line in fh_in:
-                        line = line.replace("${SSID}", ssid)
-                        line = line.replace("${PSK}", psk)
-                        fh_out.write(line)
-
-            # reload the new wpa_supplicant.conf
-            callargs = ["/sbin/wpa_cli", "reconfigure"]
-            if call(callargs) != 0:
-                raise Exception("failed to reload configuration")
-
-        except Exception as e:
-            print e
+    # Configure WiFi from TI-side
+    if os.path.exists("/tmp/wificonfig"):
+        configureWifi("/tmp/wificonfig")
+    
+    # Configure WiFi from bootstrap file on USB stick:
+    elif os.path.exists("/media/usb1/tipiwifi.txt"):
+        configureWifi("/media/usb1/tipiwifi.txt")    
         
+
+

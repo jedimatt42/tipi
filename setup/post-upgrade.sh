b00694b10a5b7805bea986eceaa49f516b336140
Matthew Splett <matt@cwfk.net>
2018-01-14 22:44:32 -0800

    maybe service scripts for tipi_monitor

--- setup/post-upgrade.sh
+++ setup/post-upgrade.sh
@@ -8,22 +8,24 @@ cd /home/tipi/tipi/setup/
 systemctl stop tipiwatchdog.service
 systemctl stop tipi.service
 systemctl stop tipioled.service
+systemctl disable tipioled.service
 systemctl stop tipiweb.service
+systemctl stop tipimon.service
 
 cp *.service /lib/systemd/system/
 
 systemctl enable tipiboot.service
 systemctl enable tipiwatchdog.service
 systemctl enable tipi.service
-systemctl enable tipioled.service
 systemctl enable tipiweb.service
+systemctl enable tipimon.service
 systemctl enable tipisuper.service
 
 systemctl restart tipiboot.service
 systemctl restart tipiwatchdog.service
 systemctl restart tipi.service
-systemctl restart tipioled.service
 systemctl restart tipiweb.service
+systemctl restart tipimon.service
 systemctl restart tipisuper.service
 
 echo "upgrade complete"

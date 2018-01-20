d513a9072d519d0ede7a380ed34be957bdbeb0af
Corey Anderson <corey@electriclab.com>
2018-01-19 16:07:00 -0800

    RawExtensions updated to support extension: 0x21, TipiVariable. Initial commit of: TipiVariable.

--- services/RawExtensions.py
+++ services/RawExtensions.py
@@ -1,14 +1,20 @@
 from Mouse import Mouse
+from TipiVariable import TipiVariable
 
+import sys
 
 class RawExtensions(object):
 
     def __init__(self, tipi_io):
         # Extend this list to register new special request handlers
-        self.__reg = {0x20: Mouse(tipi_io)}
+        #
+        self.__reg = {0x20: Mouse(tipi_io),
+                      0x21: TipiVariable(tipi_io),
+                     }
 
     def handle(self, bytes):
         if not bytes[0] in self.__reg:
             return False
         handler = self.__reg[bytes[0]]
+        
         return handler.handle(bytes)

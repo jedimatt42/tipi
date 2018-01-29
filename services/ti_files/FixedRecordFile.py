911fb179f907770ef9fb326f2f064147199a09c7
Matthew Splett <matt@cwfk.net>
2018-01-28 19:26:47 -0800

    maybe help... probably wrong

--- services/ti_files/FixedRecordFile.py
+++ services/ti_files/FixedRecordFile.py
@@ -17,7 +17,11 @@ class FixedRecordFile(object):
         self.mode = mode(pab)
         self.filetype = fileType(pab)
         self.recordLength = ti_files.recordLength(self.header)
-        self.records = self.__loadRecords(bytes[128:])
+        if self.mode == OUTPUT and self.filetype == SEQUENTIAL:
+            self.records = [ ]
+            self.dirty = True
+        else:
+            self.records = self.__loadRecords(bytes[128:])
         if self.mode == APPEND:
             self.currentRecord = len(self.records)
         else:

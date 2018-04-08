9916ec94929cf18c72d6ad5688daa69ddb16f881
Matthew Splett <matt@cwfk.net>
2018-04-08 10:22:52 -0700

    service-side mouse scaling and oled orientation

--- services/Mouse.py
+++ services/Mouse.py
@@ -1,10 +1,10 @@
 import struct
 import fcntl
 import os
+from TipiConfig import TipiConfig
 
-# Issues: reading from file is blocking IO.
-# Can't be blocking the TI-99/4A like that.
 
+tipi_config = TipiConfig.instance()
 
 class Mouse(object):
 
@@ -14,12 +14,15 @@ class Mouse(object):
         fd = self.file.fileno()
         flag = fcntl.fcntl(fd, fcntl.F_GETFL)
         fcntl.fcntl(fd, fcntl.F_SETFL, flag | os.O_NONBLOCK)
+        self.scale = float(tipi_config.get("MOUSE_SCALE", "60")) / 100.0
 
     def getMouseEvent(self):
         try:
             buf = self.file.read(3)
             button = ord(buf[0])
             x, y = struct.unpack("bb", buf[1:]);
+            x = int(x * self.scale)
+            y = int(y * self.scale)
             return bytearray(struct.pack('bbb', x, -1 * y, button))
         except IOError as e:
             return bytearray([0, 0, 0])

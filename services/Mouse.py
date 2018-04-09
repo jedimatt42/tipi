2c722983bf871040ee52764948bb52a154cbca36
Matthew Splett <matt@cwfk.net>
2018-04-08 18:24:45 -0700

    more scaling

--- services/Mouse.py
+++ services/Mouse.py
@@ -15,14 +15,23 @@ class Mouse(object):
         flag = fcntl.fcntl(fd, fcntl.F_GETFL)
         fcntl.fcntl(fd, fcntl.F_SETFL, flag | os.O_NONBLOCK)
         self.scale = float(tipi_config.get("MOUSE_SCALE", "60")) / 100.0
+        self.sf = 5
 
     def getMouseEvent(self):
         try:
             buf = self.file.read(3)
             button = ord(buf[0])
             x, y = struct.unpack("bb", buf[1:]);
-            x = int(x * self.scale)
-            y = int(y * self.scale)
+            if abs(x) > self.sf:
+                if x > 0:
+                    x = max(self.sf,int(x * self.scale))
+                else:
+                    x = min(-1 * self.sf,int(x * self.scale))
+            if abs(y) > self.sf:
+                if y > 0:
+                    y = max(self.sf,int(y * self.scale))
+                else:
+                    y = min(-1 * self.sf,int(y * self.scale))
             return bytearray(struct.pack('bbb', x, -1 * y, button))
         except IOError as e:
             return bytearray([0, 0, 0])

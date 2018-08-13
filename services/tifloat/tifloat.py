aaf75fc183fe1fdb78e532705f47aea2646e896c
Matthew Splett <matt@cwfk.net>
2018-08-12 19:47:29 -0700

    add float back to int algorithm

--- services/tifloat/tifloat.py
+++ services/tifloat/tifloat.py
@@ -34,3 +34,34 @@ def asFloat(val):
         bytes[i] = 0
 
     return bytes
+
+def asInt(bytes):
+    if bytes[0] != 8:
+        return 0
+    word0 = (bytes[1] << 8) + bytes[2]
+    word1 = bytes[3]
+
+    neg = 1
+    if (bytes[1] & 0xb0) == 0xb0:
+        neg = -1
+        word0 = ~(word0 - 1)
+
+    val = 0
+    if word0 & 0xff00 == 0x4000:
+        val = word0 & 0xff
+    if word0 & 0xff00 == 0x4100:
+        val = ((word0 & 0xff) * 100) + (word1 & 0xff)
+
+    if neg == -1:
+        val = -1 * val
+    return val 
+
+if __name__ == "__main__":
+    print asInt(asFloat(0))
+    print asInt(asFloat(1))
+    print asInt(asFloat(6))
+    print asInt(asFloat(128))
+    print asInt(asFloat(-1))
+    print asInt(asFloat(-6))
+    print asInt(asFloat(-128))
+

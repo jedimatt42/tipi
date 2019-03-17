bffaa596e91436f3d4e6664ba4e02c19de4439c4
Matthew Splett <Matthew Splett>
2019-03-16 19:04:52 -0700

    wait for disk extraction before after-upload refresh

--- htdocs/tipi_monitor.py
+++ htdocs/tipi_monitor.py
@@ -16,6 +16,18 @@ from dsks.extract_sectordump import extractDisk
 logger = logging.getLogger('tipi_monitor')
 tipi_disk = '/home/tipi/tipi_disk'
 
+tipimon_lock = '/tmp/tipimon.lock'
+
+def createLock():
+    logger.info("locking")
+    lf = open(tipimon_lock, 'w')
+    lf.write('nonsense')
+    lf.close()
+
+def releaseLock():
+    if (os.path.isfile(tipimon_lock)):
+        os.remove(tipimon_lock)
+        logger.info("released lock")
 
 def main():
     ConfigLogging.configure_logging()
@@ -28,6 +40,7 @@ def main():
 
     for event in i.event_gen():
         if event is not None:
+            createLock()
             (header, type_names, watch_path, filename) = event
             if 'IN_DELETE' in type_names:
                 name = os.path.join(watch_path.decode('utf-8'), filename.decode('utf-8'))
@@ -39,6 +52,7 @@ def main():
                     extractDisk(name)
                 else:
                     tipi_cache.updateFileInfo(name)
+            releaseLock()
 
 if __name__ == '__main__':
     main()
--- htdocs/tipi_uploads.py
+++ htdocs/tipi_uploads.py
@@ -6,6 +6,7 @@
 # et al.
 
 import os
+import time
 import logging
 from ti_files import ti_files
 from ti_files import v9t9_files
@@ -14,11 +15,23 @@ logger = logging.getLogger(__name__)
 
 tipi_disk_base = '/home/tipi/tipi_disk' 
 
+def waitForMonitor():
+    time.sleep(2)
+    checks = 3
+    while checks > 0:
+        time.sleep(1)
+        if os.path.isfile('/tmp/tipimon.lock'):
+            checks = 3
+        else:
+            checks -= 1
+        
+
 def save(path, fileset):
     for filedata in fileset:
         localfilename = os.path.join(tipi_disk_base + path, filedata.filename)
         logger.debug("saving upload to: %s", localfilename)
         filedata.save(localfilename)
         v9t9_files.convert(localfilename)
+    waitForMonitor()    
     
 

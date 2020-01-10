d263758ff1971c4df747d16b8f9c4c57ada3089f
Matthew Splett <matt@cwfk.net>
2020-01-09 18:25:50 -0800

    reformat code

--- services/TiSocket.py
+++ services/TiSocket.py
@@ -5,19 +5,24 @@ import socket
 import fcntl
 from Oled import oled
 
-# Represent socket access from Raw extensions. This is registered as 0x22 in RawExtensions.py
-#  usage: send a message starting with 0x22 as the first byte. 
+# Represent socket access from Raw extensions.
+# This is registered as 0x22 in RawExtensions.py
+#
+#  usage: send a message starting with 0x22 as the first byte.
 #   second byte should be socket handle number to use
-#   third byte is command for handle: 0x01 open, 0x02 close, 0x03 write, 0x04 read
-#   
-#   For 0x01 open / follow with string in the form of "hostname:port" 
+#   third byte is command for handle: 0x01 open, 0x02 close, 0x03 write, 0x04
+#   read.
+#
+#   For 0x01 open / follow with string in the form of "hostname:port"
 #     Tipi will return a message of 255 if connected or 0 if failed to connect.
 #   For 0x02 close / no parameters
 #     Tipi will return 255
 #   For 0x03 write / follow with bytes to write to socket
-#     Tipi will return after bytes are written with 255 or 0 if failed to write.
+#     Tipi will return after bytes are written with 255 or 0 if failed to
+#     write.
 #   For 0x04 read / follow with max size to read as int (two bytes, [msb,lsb])
-#     Tipi will return message of available socket data no greater than max size.
+#     Tipi will return message of available socket data no greater than max
+#     size.
 #
 #   socket handle numbers are a single byte 0-255
 #
@@ -27,16 +32,17 @@ logger = logging.getLogger(__name__)
 GOOD = bytearray([255])
 BAD = bytearray([0])
 
+
 class TiSocket(object):
 
     def __init__(self, tipi_io):
         self.tipi_io = tipi_io
-        self.handles = { }
+        self.handles = {}
         self.commands = {
-                0x01 : self.handleOpen,
-                0x02 : self.handleClose,
-                0x03 : self.handleWrite,
-                0x04 : self.handleRead
+                0x01: self.handleOpen,
+                0x02: self.handleClose,
+                0x03: self.handleWrite,
+                0x04: self.handleRead
                 }
 
     def handle(self, bytes):
@@ -48,7 +54,7 @@ class TiSocket(object):
             command = bytes[2]
             commandHandler = self.commands.get(command, self.defaultHandler)
             return commandHandler(bytes)
-        except IOError as e:
+        except IOError:
             return BAD
 
     def defaultHandler(self, bytes):
@@ -65,7 +71,7 @@ class TiSocket(object):
 
         existing = self.handles.get(handleId, None)
         # close the socket if we already had one for this handle.
-        if not existing is None:
+        if existing is not None:
             del(self.handles[handleId])
             self.safeClose(existing)
             existing = None
@@ -80,8 +86,9 @@ class TiSocket(object):
             logger.info("connected")
             oled.info("Socket %d/Connected", handleId)
             return GOOD
-        except Exception as e:
-            logger.info("failed to connect socket: %d", handleId, exc_info=True)
+        except Exception:
+            logger.info("failed to connect socket: %d", handleId,
+                        exc_info=True)
             self.safeClose(sock)
             return BAD
 
@@ -89,7 +96,7 @@ class TiSocket(object):
     def handleClose(self, bytes):
         handleId = bytes[1]
         existing = self.handles.get(handleId, None)
-        if not existing is None:
+        if existing is not None:
             del(self.handles[handleId])
             self.safeClose(existing)
             logger.info("closed socket: %d", handleId)
@@ -104,10 +111,11 @@ class TiSocket(object):
             return BAD
         try:
             existing.sendall(bytes[3:])
-            logger.debug("wrote %d bytes to socket: %d", len(bytes[3:]), handleId)
+            logger.debug("wrote %d bytes to socket: %d", len(bytes[3:]),
+                         handleId)
             oled.info("Socket %d/Wrote %d bytes", handleId, len(bytes[3:]))
             return GOOD
-        except Exception as e:
+        except Exception:
             del(self.handles[handleId])
             self.safeClose(existing)
             logger.info("failed to write to socket: %d", handleId)
@@ -138,11 +146,9 @@ class TiSocket(object):
             logger.error(e, exc_info=True)
         return BAD
 
-
     def safeClose(self, sock):
         try:
             sock.shutdown(socket.SHUT_RDWR)
             sock.close()
-        except Exception as e:
+        except Exception:
             pass
-

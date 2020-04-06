a582a65942dad33452940c2ce78ef14849c7e72f
Matthew Splett <jedimatt42@protonmail.com>
2020-04-05 20:57:02 -0700

    upgrade process to add tidbit

--- setup.sh
+++ setup.sh
@@ -13,6 +13,7 @@ sudo apt-get install wiringpi
 sudo apt-get install samba
 sudo apt-get install sqlite3
 sudo apt-get install libsqlite3-dev
+sudo apt-get install php
 
 if [ ! -e /home/tipi/tipi_disk ]; then
   mkdir /home/tipi/tipi_disk
@@ -22,6 +23,11 @@ if [ -d /home/tipi/xdt99 ]; then
   rm -r /home/tipi/xdt99
 fi
 ( cd /home/tipi && git clone https://github.com/endlos99/xdt99.git )
+
+if [ -d /home/tipi/tidbit ]; then
+  rm -f /home/tipi/tidbit
+fi
+( cd /home/tipi && git clone https://github.com/dnotq/tidbit.git )
 
 ( cd $TIPI/services && $TIPI/services/setup.sh )
 ( cd $TIPI/htdocs && $TIPI/htdocs/setup.sh )

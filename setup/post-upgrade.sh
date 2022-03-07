55828a9c1717b42eeadb9eb8c0200751d9ab5f95
Matthew Splett <jedimatt42@protonmail.com>
2022-03-06 16:54:04 -0800

    prepare release for js99er async fix from PeteE

--- setup/post-upgrade.sh
+++ setup/post-upgrade.sh
@@ -30,6 +30,10 @@ if [ -e /tmp/test_update ]; then
   TIPI_UPDATE_LIBTIPI=true
 fi
 
+if [ $fmajor -le 2 ] && [ $fminor -le 31 ]; then
+  TIPI_UPDATE_LIBTIPI=true
+fi
+
 if [ $fmajor -le 2 ] && [ $fminor -le 22 ]; then
   TIPI_UPDATE_DEPS=true
 fi

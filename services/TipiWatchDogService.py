e545555c4ebb24b93ccdce6a61343f175b5b6cfb
Matthew Splett <jedimatt42@protonmail.com>
2023-01-01 19:37:18 +0000

    use exported sysfs tipi_reset with irq
    make sure /dev/tipi_control and /dev/tipi_data are
    readable on boot

--- services/TipiWatchDogService.py
+++ services/TipiWatchDogService.py
@@ -1,43 +1,34 @@
 #!/usr/bin/env python3
-import RPi.GPIO as GPIO
-import sys
-import time
-import socket
 
 from subprocess import call
 
+import select
+import os
+import time
 
-class TipiWatchDog(object):
-    def __init__(self):
-        self.__RESET = 26
+tipi_reset='/sys/devices/platform/tipi_gpio/tipi-reset/'
 
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setwarnings(False)
+with open(tipi_reset + '/active_low', 'w') as edge:
+  edge.write('0')
+with open(tipi_reset + '/edge', 'w') as edge:
+  edge.write('falling')
 
-        GPIO.setup(self.__RESET, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        # Do not proceed unless the reset signal has turned off
-        # attempt to prevent restart storm in systemd
+with open(tipi_reset + '/value', 'r') as reset_value:
+  poller = select.poll()
+  poller.register(reset_value, select.POLLPRI | select.POLLERR )
 
-        print("waiting for reset to complete.")
-        while GPIO.input(self.__RESET) != 1:
-            time.sleep(0.100)
-            pass
+  poller.poll(-1)
+  reset_value.seek(0)
+  reset_value.read(1)
 
-        GPIO.add_event_detect(
-            self.__RESET, GPIO.FALLING, callback=onReset, bouncetime=100
-        )
-        print("GPIO initialized.")
-
-
-def onReset(channel):
-    print("responding to reset interrupt")
-    callargs = ["/bin/systemctl", "restart", "tipi.service"]
-    if call(callargs) != 0:
+  print("Waiting for RESET event...")
+  while True:
+    poller.poll(10000)
+    reset_value.seek(0)
+    if reset_value.read(1) == '0':
+      print("responding to reset interrupt")
+      callargs = ["/bin/systemctl", "restart", "tipi.service"]
+      if call(callargs) != 0:
         print("Error requesting restart of tipi.service")
 
 
-watchDog = TipiWatchDog()
-
-print("Waiting for RESET event...")
-while True:
-    time.sleep(1)

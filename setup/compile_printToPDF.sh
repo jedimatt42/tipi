5db9083c6b1133772fb965d7fc59b840cda32021
Matthew Splett <jedimatt42@protonmail.com>
2023-01-29 11:14:30 -0800

    my aarch64 fix for PrinterToPDF was accepted

--- setup/compile_printToPDF.sh
+++ setup/compile_printToPDF.sh
@@ -6,10 +6,8 @@ function build() {
   then
     rm -fr /home/tipi/PrinterToPDF
   fi
-  # git clone https://github.com/RWAP/PrinterToPDF.git
-  git clone https://github.com/jedimatt42/PrinterToPDF.git
+  git clone https://github.com/RWAP/PrinterToPDF.git
   cd PrinterToPDF
-  git checkout jm42/aarch64_support
   git pull
   make printerToPDF
 }

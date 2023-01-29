811a9c882da2718bce21537fd78556f3bebed4ce
Matthew Splett <jedimatt42@protonmail.com>
2023-01-28 17:56:39 -0800

    work around for 64bit compilation

--- setup/compile_printToPDF.sh
+++ setup/compile_printToPDF.sh
@@ -6,8 +6,10 @@ function build() {
   then
     rm -fr /home/tipi/PrinterToPDF
   fi
-  git clone https://github.com/RWAP/PrinterToPDF.git
+  # git clone https://github.com/RWAP/PrinterToPDF.git
+  git clone https://github.com/jedimatt42/PrinterToPDF.git
   cd PrinterToPDF
+  git checkout jm42/aarch64_support
   git pull
   make printerToPDF
 }

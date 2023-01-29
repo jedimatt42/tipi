03d6f3dccdf333bcdb99ba4485d05edb3c278104
Matthew Splett <jedimatt42@protonmail.com>
2023-01-28 16:50:45 -0800

    temporary while figuring out edge detection
    On the Le Potato, the kernel module didn't enable
    the edge detection control file... hmmm

--- services/TipiWatchDogService.py
+++ services/TipiWatchDogService.py
@@ -8,24 +8,27 @@ import time
 
 tipi_reset='/sys/devices/platform/tipi_gpio/tipi-reset/'
 
-with open(tipi_reset + '/active_low', 'w') as edge:
-  edge.write('0')
-with open(tipi_reset + '/edge', 'w') as edge:
-  edge.write('falling')
+with open(tipi_reset + '/active_low', 'w') as f_active_low:
+  f_active_low.write('0')
+try: 
+  with open(tipi_reset + '/edge', 'w') as f_edge:
+    f_edge.write('falling')
+except:
+    print("WARN: could not set trigger on falling edge")
 
-with open(tipi_reset + '/value', 'r') as reset_value:
+with open(tipi_reset + '/value', 'r') as f_reset_value:
   poller = select.poll()
-  poller.register(reset_value, select.POLLPRI | select.POLLERR )
+  poller.register(f_reset_value, select.POLLPRI | select.POLLERR )
 
   poller.poll(-1)
-  reset_value.seek(0)
-  reset_value.read(1)
+  f_reset_value.seek(0)
+  f_reset_value.read(1)
 
   print("Waiting for RESET event...")
   while True:
     poller.poll(10000)
-    reset_value.seek(0)
-    if reset_value.read(1) == '0':
+    f_reset_value.seek(0)
+    if f_reset_value.read(1) == '0':
       print("responding to reset interrupt")
       callargs = ["/bin/systemctl", "restart", "tipi.service"]
       if call(callargs) != 0:

6fd7ae8a81a4e76963f2fe651fab4f1d0561dd57
Matthew Splett <jedimatt42@protonmail.com>
2025-02-21 20:42:39 -0800

    rought keyboard plugin

--- examples/msg_plugin/61.py
+++ examples/msg_plugin/61.py
@@ -1,29 +1,51 @@
 import logging
 import evdev
+import threading
+import queue
 
 logger = logging.getLogger(__name__)
-
-
-
-# When installed in /home/tipi/tipi_disk/PLUGINS as '60.py', this plugin will respond to 
-# TIPI message requests for message type 0x60. 
 
 
 class KeyboardPlugin(object):
     def __init__(self):
         logger.info('created plugin instance')
+        self.device = None
+        self.queue = queue.Queue()
+        self.thread = None
+        self.grabbed = False
 
-    # 'bytes' will be a byte array of the message sent to the plugin
-    # 'bytes' includes the message type byte at index 0. 
+    def grab_keyboard(self):
+        devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
+        for device in devices:
+            logger.info(f'examining device: {device.name}')
+            if 'keyboard' in device.name.lower():
+                self.device = device
+                self.device.grab()
+                logger.info(f'grabbed keyboard: {device.name}')
+                break
+
+    def read_keyboard(self):
+        for event in self.device.read_loop():
+            if event.type == evdev.ecodes.EV_KEY and event.value == 1:  # Key press event
+                self.queue.put(event.code)
+                logger.info(f'key pressed: {evdev.ecodes.KEY[event.code]}')
+
     def handle(self, bytes):
-        # process the message and send a message back
-        # processing should catch any exceptions and return an error code or empty message back to the 4A
         try:
-            logger.info('handled plugin message')
-            # return a message containing the letter 'A'
-            return [0x41]
-        except:
-            logger.error('something went wrong')
-            # Always return something, even 0 length array of bytes, or the 4A/client side will hang
+            if not self.grabbed:
+                self.grab_keyboard()
+                self.thread = threading.Thread(target=self.read_keyboard, daemon=True)
+                self.thread.start()
+                self.grabbed = True
+
+            if not self.queue.empty():
+                key_code = self.queue.get()
+                logger.info(f'handled plugin message, key code: {key_code}')
+                return [key_code]
+            else:
+                return []
+
+        except Exception as e:
+            logger.error(f'something went wrong: {e}')
             return []
 
